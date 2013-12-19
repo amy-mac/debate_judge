f22fab27e9ad0730c806e176e46d238c0ffdb931
Amy MacKinnon <amy@amy-mac.com>
2013-12-18 22:30:02 -0800

    More CSS styling

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,8 @@ class MainController < ApplicationController
 
   def index
     gon.currentUser = current_user
+    if current_user
+      gon.userRounds = current_user.rounds.last(5)
+    end
   end
 end
