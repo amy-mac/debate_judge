da3e3a12010af11e1b757cc2a4d0aeb077211a33
Amy MacKinnon <amy@amy-mac.com>
2014-01-13 15:37:10 -0800

    Cleaning up old code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,3 @@
 module ApplicationHelper
-  def markdown(text)
-    renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
-    options = {
-      autolink: true,
-      no_intra_emphasis: true,
-      lax_html_blocks: true,
-      strikethrough: true,
-      superscript: true,
-      quote: true,
-      highlight: true,
-      underline: true
-    }
-    Redcarpet::Markdown.new(renderer, options).render(text).html_safe
-  end
+
 end
