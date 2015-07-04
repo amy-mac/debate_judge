fada92fccd21b304f1b1c1b5be4fa9ed1b11acaa
Amy MacKinnon <amy@amy-mac.com>
2015-07-04 14:46:46 -0700

    Replaced User model coffee file with js

---
+++ app/assets/javascripts/models/user.js
@@ -0,0 +1,28 @@
+DebateJudge.Models.User = Backbone.Model.extend({
+  urlRoot: '/api/users',
+  paramRoot: 'user',
+
+  initialize: function() {
+    console.log("This model has been initialized.");
+  },
+
+  validate: function(attrs) {
+    this.errors = [];
+
+    if (attrs.password.length < 6) {
+      this.errors.push("Password must be at least 6 characters");
+    }
+
+    if (attrs.password !== attrs.password_confirmation) {
+      this.errors.push("Password and Password Confirmation do not match");
+    }
+
+    if (!_.isEmpty(this.errors)) {
+      return this.errors;
+    }
+  },
+
+  editInfo: function(attrs) {
+    this.save(attrs, {patch: true});
+  }
+});
