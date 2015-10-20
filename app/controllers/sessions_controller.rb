af9971beb6156a248bedea1f8e41d81ac65f9a8c
Amy MacKinnon <amy@amy-mac.com>
2015-10-20 11:33:00 -0700

    Added better error messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,22 +2,30 @@ class SessionsController < ApplicationController
   skip_before_filter :authorize, only: [:new, :create]
   respond_to :html, :json
 
-  def create
-    user = User.find_by_email(params[:email])
+  def new
+    gon.currentUserID = current_user.try(:id)
+  end
 
-    if user && user.authenticate(params[:password])
-      sign_in(user)
-      respond_with user
-    elsif !user
-      # TODO: Fix the possible nil value here
-      respond_with User.find_by_email(params[:email])
+  def create
+    @user = User.find_by_email(params[:email])
+
+    unless @user
+      flash.now[:error] = "A user with that email does not exist."
+      render action: "new"
+      return
+    end
+
+    if @user && @user.authenticate(params[:password])
+      sign_in(@user, params[:remember])
+      redirect_to tournaments_path, :notice => "You have successfully logged in."
     else
-      respond_with user.authenticate(params[:password])
+      flash.now[:error] = "Password is incorrect."
+      render action: "new"
     end
   end
 
   def destroy
     sign_out
-    redirect_to root_path, :notice => "Logged out"
+    redirect_to root_path, :notice => "You have successfully logged out."
   end
 end
